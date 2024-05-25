dcd6bf830cf688997faaa861b92a6d8ddfcbb4a6
akash9753 <akash9753@gmail.com>
2024-05-25 23:22:58 +0530

    updated

--- server.js
+++ server.js
@@ -1,23 +1,29 @@
-dotenv.config();
-import express from "express";
-import dotenv from "dotenv";
+dotenv.config()
+import express from "express"
+import dotenv from "dotenv"
 
-import connectDB from "./config/db.js";
 
-import cors from "cors";
-const port = process.env.PORT || 8000;
-connectDB();
+import connectDB from "./config/db.js"
+
+
+
+import cors from 'cors';
+const PORT = process.env.PORT || 8000
+connectDB()
 const app = express();
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("working...............");
-});
 
-app.listen(port, () => {
+app.get('/',(req,res)=>{
+  res.send("working...............")
+})
+
+
+
+
+app.listen(PORT,()=>{
   console.log(`server started at port 7000`);
-});
+})
